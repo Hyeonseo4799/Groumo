add70eef4ef0601f900099c68a94c3e1239e327e
hyeonseo <s21035@gsm.hs.kr>
2023-02-01 11:29:05 +0900

    Init

---
+++ Groumo-Android/app/build.gradle.kts
@@ -0,0 +1,47 @@
+@file:Suppress("DSL_SCOPE_VIOLATION")
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin)
+}
+
+android {
+    namespace = "com.ragdoll.groumo"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "com.ragdoll.groumo"
+        minSdk = 21
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.4.0"
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.android.material)
+    implementation(libs.androidx.compose.runtime)
+    implementation(libs.androidx.activity)
+}
