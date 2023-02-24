662a7ffef982e9429aa642e9a226c76fff3123a4
hyeonseo <s21035@gsm.hs.kr>
2023-02-24 19:06:58 +0900

    Update: 카카오 로그인과 회원가입 로직 분리

--- Groumo-Android/feature/login/src/main/java/com/ragdoll/login/LoginViewModel.kt
+++ Groumo-Android/feature/login/src/main/java/com/ragdoll/login/LoginViewModel.kt
@@ -3,6 +3,8 @@ package com.ragdoll.login
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
+import com.kakao.sdk.auth.AuthApiClient
+import com.kakao.sdk.auth.TokenManagerProvider
 import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.user.UserApiClient
 import com.ragdoll.common.result.Result
@@ -24,23 +26,38 @@ class LoginViewModel @Inject constructor(
     private val _uiState = MutableStateFlow<UserUiState>(UserUiState.Loading)
     val uiState: StateFlow<UserUiState> = _uiState.asStateFlow()
 
-    fun kakaoLogin() {
-        val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
-            when {
-                error != null -> _uiState.value = UserUiState.Error(error.localizedMessage)
-                token != null -> {
-                    viewModelScope.launch {
-                        signUpUseCase(token.accessToken, "kakao").asResult().collect { result ->
-                            when (result) {
-                                is Result.Success -> _uiState.value = UserUiState.Success(result.data)
-                                is Result.Error -> _uiState.value = UserUiState.Error(result.message)
-                                Result.Loading -> _uiState.value = UserUiState.Loading
-                            }
-                        }
-                    }
+    init { checkToken() }
+
+    private fun checkToken() {
+        if (AuthApiClient.instance.hasToken()) {
+            UserApiClient.instance.accessTokenInfo { _, error ->
+                if (error == null) {
+                    val token = TokenManagerProvider.instance.manager.getToken()
+                    signUp(token!!.accessToken)
                 }
             }
         }
+    }
+
+    fun loginWithKakaoAccount() {
+        val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+            when {
+                error != null -> _uiState.value = UserUiState.Error(error.localizedMessage)
+                token != null -> signUp(token.accessToken)
+            }
+        }
         UserApiClient.instance.loginWithKakaoAccount(context, callback = callback)
+    }
+
+    private fun signUp(token: String) {
+        viewModelScope.launch {
+            signUpUseCase(token, "kakao").asResult().collect { result ->
+                when (result) {
+                    is Result.Success -> _uiState.value = UserUiState.Success(result.data)
+                    is Result.Error -> _uiState.value = UserUiState.Error(result.message)
+                    Result.Loading -> _uiState.value = UserUiState.Loading
+                }
+            }
+        }
     }
 }
