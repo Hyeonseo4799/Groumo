2f4e563e5a97e0534b631f6efddeeade70cb7181
hyeonseo <s21035@gsm.hs.kr>
2023-03-02 16:21:08 +0900

    Update: 텍스트 가운데 정렬 추가

--- Groumo-Android/core/designsystem/src/main/java/com/ragdoll/designsystem/component/Error.kt
+++ Groumo-Android/core/designsystem/src/main/java/com/ragdoll/designsystem/component/Error.kt
@@ -1,29 +1,35 @@
 package com.ragdoll.designsystem.component
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.ragdoll.designsystem.R
 import com.ragdoll.designsystem.theme.lineSeed
 
 @Composable
 fun Error(message: String?) {
-    Column {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         Text(
             text = message ?: stringResource(R.string.unknown_error),
             color = Color.Red,
-            fontFamily = lineSeed
+            fontFamily = lineSeed,
+            textAlign = TextAlign.Center
         )
         Spacer(modifier = Modifier.height(10.dp))
         Text(
             text = stringResource(R.string.error),
-            fontFamily = lineSeed
+            fontFamily = lineSeed,
+            textAlign = TextAlign.Center
         )
     }
 }
