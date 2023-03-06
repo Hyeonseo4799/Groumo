f6f5b0f403161f1d8ab4728e8beee1915fcb3707
hyeonseo <s21035@gsm.hs.kr>
2023-03-06 14:41:06 +0900

    Update: uistate 파라미터 이름 변경

--- Groumo-Android/feature/group/src/main/java/com/ragdoll/group/GroupUiState.kt
+++ Groumo-Android/feature/group/src/main/java/com/ragdoll/group/GroupUiState.kt
@@ -5,5 +5,5 @@ import com.ragdoll.model.Group
 sealed interface GroupUiState {
     object Loading : GroupUiState
     data class Error(val message: String?) : GroupUiState
-    data class Success(val user: List<Group>) : GroupUiState
+    data class Success(val group: List<Group>) : GroupUiState
 }
