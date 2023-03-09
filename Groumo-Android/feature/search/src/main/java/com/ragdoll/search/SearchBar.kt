c8b2ecc3d37c022cbc0396d2645a632d89a107c1
hyeonseo <s21035@gsm.hs.kr>
2023-03-09 20:46:11 +0900

    Update: 사용자 테마에 맞도록 색상 변경

--- Groumo-Android/feature/search/src/main/java/com/ragdoll/search/SearchBar.kt
+++ Groumo-Android/feature/search/src/main/java/com/ragdoll/search/SearchBar.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -37,10 +38,14 @@ fun SearchBar(
             .padding(vertical = 14.dp)
             .border(1.dp, MaterialTheme.colorScheme.primary, RoundedCornerShape(20.dp))
             .background(MaterialTheme.colorScheme.background, RoundedCornerShape(20.dp))
-            .padding(horizontal = 10.dp, vertical = 6.dp),
+            .padding(horizontal = 10.dp, vertical = 8.dp),
         singleLine = true,
         enabled = true,
-        textStyle = TextStyle(fontSize = 14.sp, fontFamily = lineSeed),
+        textStyle = TextStyle(
+            fontSize = 14.sp,
+            fontFamily = lineSeed,
+            color = MaterialTheme.colorScheme.onBackground
+        ),
         decorationBox = { innerTextField ->
             Row(
                 modifier = modifier.fillMaxWidth(),
@@ -53,6 +58,7 @@ fun SearchBar(
                 )
                 innerTextField()
             }
-        }
+        },
+        cursorBrush = SolidColor(MaterialTheme.colorScheme.onBackground)
     )
 }
